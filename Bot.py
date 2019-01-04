0ce7616c55d2160f9e3e60ab41616196d299f1f3
Anthony <anthonykutz57@gmail.com>
2019-01-03 20:54:24 -0800

    Now we are ready for pull into master

--- Bot.py
+++ Bot.py
@@ -19,36 +19,14 @@ async def on_ready():
     print("------")
 
 
-@client.command(pass_context=True)
-async def s(ctx, stuff=""):
-    """Send Status"""
+# @client.command(pass_context=True)
+# async def playing_dayz(ctx, stuff=""):
+#     """Displays the current members"""
+#     for member in client.get_all_members():
+#         if str(member.game) == "DayZ":
+#             await client.say("@" + str(member) + " is playing " + str(member.game))
+#     await client.say("Done")
 
-    url = 'https://www.gametracker.com/server_info/108.61.118.183:2302/'
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    content = urllib.request.urlopen(req).read()
-
-    soup = BeautifulSoup(content, features="html.parser")
-
-    last_scan = soup.find("div", {"id": "last_scanned"})
-    response = requests.get("http://cache.gametracker.com/server_info/"
-                            "108.61.118.183:2302/b_560_95_1.png")
-    pic = Image.open(BytesIO(response.content))
-
-    pic.save("pic.png")
-    # print(testImage)
-    await client.upload("pic.png")
-    await client.say(last_scan.text)
-
-#todo: Add a function that sends the user links to the 5 most recent blog post on dayz dev-hub
-
-
-@client.command(pass_context=True)
-async def playing_dayz(ctx, stuff=""):
-    """Displays the current members"""
-    for member in client.get_all_members():
-        if (str(member.game) == "DayZ"):
-            await client.say("@" + str(member) + " is playing " + str(member.game))
-    await client.say("Done")
 
 @client.command(pass_context=True)
 async def status(ctx, stuff=""):
@@ -68,8 +46,11 @@ async def status(ctx, stuff=""):
     # print(testImage)
     await client.upload("pic.png")
     await client.say(last_scan.text)
+
+
 @client.command(pass_context=True)
 async def ip(ctx, stuff=""):
     await client.say("Server IP: `108.61.118.183:2302`")
+
 
 client.run(TOKEN)
