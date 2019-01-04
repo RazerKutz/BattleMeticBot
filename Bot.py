f917f585122004d70ec44b498c242bfc42ee5c52
Anthony <anthonykutz57@gmail.com>
2019-01-03 20:28:34 -0800

    Added an ip command

--- Bot.py
+++ Bot.py
@@ -1,21 +1,43 @@
 import config
 import requests
 from io import BytesIO
-from  bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from PIL import Image
 import urllib.request
 from discord.ext import commands
 
 TOKEN = config.beta_app_token
-BOT_PREFIX = "!"
+BOT_PREFIX = "?"
 client = commands.Bot(command_prefix=commands.when_mentioned_or(BOT_PREFIX))
+
 
 @client.event
 async def on_ready():
-    print ("Logged in as")
-    print (client.user.name)
-    print (client.user.id)
-    print ("------")
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------")
+
+
+@client.command(pass_context=True)
+async def s(ctx, stuff=""):
+    """Send Status"""
+
+    url = 'https://www.gametracker.com/server_info/108.61.118.183:2302/'
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    content = urllib.request.urlopen(req).read()
+
+    soup = BeautifulSoup(content, features="html.parser")
+
+    last_scan = soup.find("div", {"id": "last_scanned"})
+    response = requests.get("http://cache.gametracker.com/server_info/"
+                            "108.61.118.183:2302/b_560_95_1.png")
+    pic = Image.open(BytesIO(response.content))
+
+    pic.save("pic.png")
+    # print(testImage)
+    await client.upload("pic.png")
+    await client.say(last_scan.text)
 
 
 @client.command(pass_context=True)
@@ -36,4 +58,8 @@ async def status(ctx, stuff=""):
     # print(testImage)
     await client.upload("pic.png")
     await client.say(last_scan.text)
+@client.command(pass_context=True)
+async def ip(ctx, stuff=""):
+    await client.say("Server IP: `108.61.118.183:2302`")
+
 client.run(TOKEN)
